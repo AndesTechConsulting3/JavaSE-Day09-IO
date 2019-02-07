2991bcf9b224cc00f8c4bc50a1e0c45d8234c87a
a0z9 <a0z9@rambler.ru>
2019-02-07 16:53:29 +0300

    1

---
+++ src/main/java/org/andestech/learning/rfb19/g3/AppFIO.java
@@ -0,0 +1,68 @@
+package org.andestech.learning.rfb19.g3;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class AppFIO {
+
+    public static void main(String[] args)
+    {
+
+        File file = new File("e:\\datas3\\file2");
+
+
+       // if(file.exists())
+        try {
+            boolean res = file.createNewFile();
+            System.out.println("success? " + res);
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+
+        }
+
+        file = new File("e:\\datas3\\file2");
+        boolean res = file.delete();
+        System.out.println("delete success? " + res);
+
+        file = new File("e:\\datas3\\inner1\\inner2");
+       // if(file.mkdirs()) {}
+        file.mkdirs();
+
+
+        // Simple File IO
+
+        File file2 = new File("e:\\datas3\\file2.txt");
+
+
+        FileWriter fw = null;
+        try
+        {
+            fw = new FileWriter(file2);
+            fw.write("Test string\r\nТестовая строка!!!");
+
+            fw.close();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();}
+        finally {
+            if(fw != null)
+                try{fw.close();}
+                catch (IOException ex){ex.printStackTrace();}
+        }
+
+
+        try(FileWriter fw1 = new FileWriter("e:\\datas3\\file3.txt"))
+        {
+            fw1.write("Salute!!\r\nСалют!!");
+
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+
+    }
+}
